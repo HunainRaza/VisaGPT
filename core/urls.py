db0662fb287f90a380dcaf7ffff76f2ca9505245
Hunain Raza <hunain.a4@gmail.com>
2025-12-23 22:34:21 +0100

    URL updated for media files

--- core/urls.py
+++ core/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('predictions/', include('predictions.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
